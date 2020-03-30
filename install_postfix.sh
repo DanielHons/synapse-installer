dbb4d70b0030a61a09da383c6896c48591be5a59
Daniel Hons <mail@danielhons.de>
2020-03-30 18:02:12 +0200

    Add server restart after postfix installation

--- install_postfix.sh
+++ install_postfix.sh
@@ -11,3 +11,7 @@ sed -i -e "s/matrix.example.com/${DOMAIN}/g" /etc/postfix/main.cf
 sed -i -e "s/#notif_from:/notif_from:/g" ${VIRTUAL_ENV_DIR}/homeserver.yaml
 
 /etc/init.d/postfix restart
+
+cd ${VIRTUAL_ENV_DIR}
+source env/bin/activate
+synctl start
